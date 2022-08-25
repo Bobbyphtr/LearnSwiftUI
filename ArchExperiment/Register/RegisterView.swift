faef64ca8bdcd31a8e0a2843245497e59c9399a6
Bobbyphtr <bobbypehtrus2@gmail.com>
2022-08-25 18:37:41 +0700

    Changed from actions into interactor

--- ArchExperiment/Register/RegisterView.swift
+++ ArchExperiment/Register/RegisterView.swift
@@ -9,54 +9,60 @@ import Combine
 import SwiftUI
 
 struct RegisterView: View {
-        
-    let action: RegisterViewActions = RegisterViewActions()
+    
+    let interactor: RegisterViewInteractor = RegisterViewInteractor()
     
     @ObservedObject var viewModel : RegisterViewModel
     
     var usernameText: AnyCancellable?
     
     var body: some View {
-            VStack(spacing: 16.0) {
+        VStack(spacing: 16.0) {
             
-                Text("Register")
-                    .font(.title)
-                
-                TVLViewSwiftUI { _ in
-                    let tf = CustomUITextField()
-                    tf.placeholder = "Username"
-                    tf.errorText = "This is username error text."
-                    tf.translatesAutoresizingMaskIntoConstraints = false
-                    tf.textfield.addTarget(action, action: #selector(action.onUsernameFieldDidChanged(_:)), for: .editingChanged)
-                    action.onUsernameFieldDidChanged = { [weak viewModel] fieldText in
-                        viewModel?.onUsernameFieldChanged(fieldText.text ?? "")
-                    }
-                    return tf
+            Text("Register")
+                .font(.title)
+            
+            TVLViewSwiftUI { _ in
+                let tf = CustomUITextField()
+                tf.placeholder = "Username"
+                tf.errorText = "This is username error text."
+                tf.translatesAutoresizingMaskIntoConstraints = false
+                tf.textfield.addTarget(interactor, action: #selector(interactor.onUsernameFieldDidChanged(_:)), for: .editingChanged)
+                interactor.onUsernameFieldDidChanged = { [weak viewModel] fieldText in
+                    viewModel?.onUsernameFieldChanged(fieldText.text ?? "")
                 }
-                .fixedSize(horizontal: false, vertical: true)
-                
-                TVLViewSwiftUI ({ context in
-                    let customTf = CustomUITextField()
-                    customTf.placeholder = "Password"
-                    customTf.errorText = "Password Error text"
-                    customTf.translatesAutoresizingMaskIntoConstraints = false
-                    return customTf
-                })
-                .fixedSize(horizontal: false, vertical: true)
-
-                TVLViewSwiftUI { _ in
-                    let btn = CustomButton(style: .primary)
-                    btn.setTitle("Register", for: .normal)
-                    return btn
-                }
-                .fixedSize()
+                return tf
             }
-            .padding(.horizontal, 16.0)
-     
+            .fixedSize(horizontal: false, vertical: true)
+            
+            TVLViewSwiftUI ({ context in
+                let customTf = CustomUITextField()
+                customTf.placeholder = "Password"
+                customTf.errorText = "Password Error text"
+                customTf.translatesAutoresizingMaskIntoConstraints = false
+                return customTf
+            })
+            .fixedSize(horizontal: false, vertical: true)
+            
+            TVLViewSwiftUI { _ in
+                let btn = CustomButton(style: .primary)
+                btn.setTitle("Register", for: .normal)
+                return btn
+            }
+            .fixedSize()
         }
+        .onAppear(perform: {
+            interactor.viewModel = viewModel
+        })
+        .padding(.horizontal, 16.0)
+        
+    }
     
-    class RegisterViewActions: NSObject, UITextFieldDelegate {
-
+    class RegisterViewInteractor: NSObject, UITextFieldDelegate {
+        
+        // This is optional to have. But it could make things easier.
+        weak var viewModel: RegisterViewModel?
+        
         var onUsernameFieldDidChanged: ((_ textfield: UITextField) -> Void)?
         
         @objc func onUsernameFieldDidChanged(_ textfield: UITextField) {
